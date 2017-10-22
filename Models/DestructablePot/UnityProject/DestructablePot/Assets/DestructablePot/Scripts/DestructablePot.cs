ca8e36494f8125860b3cc1a5856d47e1bbfbf2aa
peroon <fromjpn@gmail.com>
2017-10-22 23:49:18 +0900

    Add Unity Project

---
+++ Models/DestructablePot/UnityProject/DestructablePot/Assets/DestructablePot/Scripts/DestructablePot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructablePot : MonoBehaviour {
+
+    public GameObject brokenPrefab;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var brokenObject = Instantiate(brokenPrefab);
+        brokenObject.transform.position = this.gameObject.transform.position;
+        brokenObject.transform.rotation = this.gameObject.transform.rotation;
+
+        Destroy(this.gameObject);    
+    }
+}
