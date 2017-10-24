83118efd0e9a65a7c8821d5924d41998bb2fccab
peroon <fromjpn@gmail.com>
2017-10-24 14:32:46 +0900

    [Unity]Add sample scene

--- Models/DestructablePot/UnityProject/DestructablePot/Assets/DestructablePot/Scripts/DestructablePot.cs
+++ Models/DestructablePot/UnityProject/DestructablePot/Assets/DestructablePot/Scripts/DestructablePot.cs
@@ -2,16 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// sample
+/// </summary>
+
 public class DestructablePot : MonoBehaviour {
 
     public GameObject brokenPrefab;
+    public AudioSource breakSound;
 
     private void OnCollisionEnter(Collision collision)
     {
         var brokenObject = Instantiate(brokenPrefab);
         brokenObject.transform.position = this.gameObject.transform.position;
         brokenObject.transform.rotation = this.gameObject.transform.rotation;
+        brokenObject.transform.localScale = this.gameObject.transform.localScale;
 
-        Destroy(this.gameObject);    
+        Destroy(this.gameObject);
+
+        this.breakSound.Play();
     }
 }
